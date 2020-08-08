b653d3c6a7924d40d0e9bc392abb0de9c16cc7b3
hansonjw <hansonjw@gmail.com>
2020-08-08 18:25:10 -0500

    Add many features including complexity option radio buttons, text input for password size, etc.

--- script.js
+++ script.js
@@ -6,49 +6,91 @@ var generateBtn = document.querySelector("#generate");
 //
 // Options for types of characters to include:
 // lowercase, uppercase, numeric, and/or special characters
-// Validate user input -> loop if no options selected
 
-// Generate string of characters to inlcude in the password based on user input
-const alphabetLower = "abcdefghijklmnopqrstuvwxyz";
-var alphabetUpper = alphabetLower.toUpperCase();
-const numbers = "0123456789";
-const specialChars = "@#$%&*";
-var passWordLength = 8;
-var charOptions = alphabetLower+alphabetUpper+numbers+specialChars;
-
-
-function generateRandomNumber (min, max){
-  //This function generates a random number between min and max (inclusive)
+function generateRandomNumber (min, max) {
+  //This function generates a random number between (inclusive) min and max
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function generatePassword(aList, aSize){
-  // Generate a password of length aSize from the characters in aList
-  var passWord = ""; // initialize an empty string to be used in the following loop
-  for (i = 0; i < aSize; i++){
-    passWord = passWord + aList[generateRandomNumber(0, aList.length)];
-    // debugging - delete later
-    console.log(passWord);
+function validateInputs() {
+
+  if (document.getElementById("Low").checked == false &&
+    document.getElementById("Up").checked == false &&
+    document.getElementById("Num").checked == false &&
+    document.getElementById("Spec").checked == false)
+  {
+    return false;
+  }
+
+  var inputLength = parseInt(document.getElementById("PassWordLength").value);
+
+  if (!Number.isInteger(inputLength)){
+    return false;
+  }
+  if (inputLength < 8 || inputLength > 128){
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+function generatePassword() {
+  // Generate a password of length and character sets from page
+  var inputLength = parseInt(document.getElementById("PassWordLength").value);
+
+  const alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const specialChars = "@#$%&*";
+
+  // Generate a string of characters to generate a password from
+  // based on the user inputs
+  var charOptions = "";
+
+  if (document.getElementById("Low").checked == true){
+    charOptions = charOptions + alphabetLower;
+  }
+  if (document.getElementById("Up").checked == true){
+    charOptions = charOptions + alphabetUpper;
+  }
+  if (document.getElementById("Num").checked == true){
+    charOptions = charOptions + numbers;
+  }
+  if (document.getElementById("Spec").checked == true){
+    charOptions = charOptions + specialChars;
+  }
+
+  // initialize an empty string that will become the password
+  // iterate and generate the password based on 
+  var passWord = "";
+  for (i = 0; i < inputLength; i++){
+    passWord = passWord + charOptions[generateRandomNumber(1, charOptions.length)];
   }
   return passWord;
 }
 
-
-
 // Write password to the #password input
 function writePassword() {
 
-  // call some other function to display dropdowns, etc and get input
+  // Call function to validate inputs before proceding 
+  var inputStatus = validateInputs();
+  // If inputs are not valid, reset forms and return out of the function before generating a password ;)
+  if (inputStatus == false){
+    alert("Password length must be an integer between 8 and 128 characters, and you must choose a complexity.");
+    document.getElementById("Low").checked = false;
+    document.getElementById("Up").checked = false;
+    document.getElementById("Num").checked = false;
+    document.getElementById("Spec").checked = false;
+    document.getElementById("PWLength").reset();
+    //generatePassword.addEventListener("click", writePassword);
+    return;
+  }
 
-
-  var password = generatePassword(charOptions, passWordLength);
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  //debugging stuff
-  console.log("justin the function was called when the button was pushed");
-  console.log(password);
 
 }
 
